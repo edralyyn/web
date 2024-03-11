2bc712fcb32aecfd2c5586b08146774bc1f5d1f8
edralyyn <qerperalta@tip.edu.ph>
2024-03-11 13:30:48 +0800

    background

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import Navbar from './Navbar';
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
+
+function Model(props) {
+  const { scene } = useGLTF("/bmw.glb");
+  return <primitive object={scene} {...props} />
+}
 
 const Homepage = () => {
     return (
         <div>
             <Navbar/>
+            <div>
+            <Canvas dpr={[1,2]} shadows camera={{ fov: 45 }} style={{"position": "absolute", "touchAction": "none"}}>
+  <color attach="background" args={["#101010"]} />
+  <PresentationControls speed={1.5} global zoom={.5} polar={[-0.1, Math.PI / 4]}>
+    <Stage environment={"sunset"}>
+      <Model scale={0.005}/>
+    </Stage>
+  </PresentationControls>
+</Canvas>
+
+    </div>
         </div>
     );
 }
