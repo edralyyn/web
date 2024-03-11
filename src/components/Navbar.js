88979f3f943b1341861682209a5a4c33b916fa54
edralyyn <qerperalta@tip.edu.ph>
2024-03-11 13:37:43 +0800

    buttons not working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,8 +3,9 @@ import React from 'react';
 const Navbar = () => {
     return (
         <nav className="navbar navbar-dark fixed-top bg-dark rounded-2 m-2">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Fixed top</a>
+            <div className="d-grid gap-2 d-md-block">
+            <button type="button" className="btn btn-light btn-sm m-2 mt-0 mb-0">Small button</button>
+            <button type="button" className="btn btn-light btn-sm m-2 mt-0 mb-0">Small button</button>
             </div>
         </nav>
     );
